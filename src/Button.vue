107385e0258abd12dff77a17b201f2af2cf43a55
TITanZK <503838819@qq.com>
2021-03-28 14:45:47 +0800

    Button 添加 icon iconPosition属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,10 +1,18 @@
 <template>
-  <button class="d-button">按钮</button>
+  <button class="d-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-  name: "Button"
+  name: "Button",
+  props: ['icon', 'iconPosition']
 }
 </script>
 
@@ -16,6 +24,10 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  vertical-align: middle;//css真的贱，不对齐是真的烦
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     border-color: var(--border-color-hover);
@@ -27,6 +39,26 @@ export default {
 
   &:focus {
     outline: none;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .3em
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em;
+    }
+    >.content{
+      order: 1;
+    }
   }
 }
 
