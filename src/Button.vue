c94db0e3aa865f6cc8df809fd3190e745ba3bb91
TITanZK <503838819@qq.com>
2021-03-28 13:34:04 +0800

    改用 vue 模板

---
+++ src/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="d-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+}
+</script>
+
+<style scoped lang="scss">
+.d-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    border-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from "./Button"
+
+Vue.component('d-button',Button)
+
+new Vue({
+  el: '#app',
+})
