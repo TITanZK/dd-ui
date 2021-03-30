f8520d128e4e369e732520c942283afc04072109
TITanZK <503838819@qq.com>
2021-03-30 17:07:46 +0800

    布局基本实现
    Signed-off-by: TITanZK <503838819@qq.com>

---
+++ src/Col.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col"
+       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+       :class="[span&&`col-${span}`,offset&&`offset-${offset}`]">
+    <div style="border: 1px solid green;">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Col",
+  props: {
+    span: {type: [Number, String]},
+    offset: {type: [Number, String]}
+  },
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.col {
+  height: 100px;
+  width: 50%;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24)*100%
+    }
+  }
+
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24)*100%
+    }
+  }
+}
+</style>
