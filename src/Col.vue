454798736c1917a2dc4fdb600dd2f6aea0851ab0
TITanZK <503838819@qq.com>
2021-03-30 20:47:00 +0800

    默认手机样式

--- src/Col.vue
+++ src/Col.vue
@@ -20,7 +20,6 @@ export default {
   props: {
     span: {type: [Number, String]},
     offset: {type: [Number, String]},
-    phone: {type: Object, validator},
     ipad: {type: Object, validator},
     narrowPc: {type: Object, validator},
     pc: {type: Object, validator},
@@ -31,11 +30,10 @@ export default {
   },
   computed: {
     colClass() {
-      let {span, offset, phone,ipad,narrowPc,pc,widePc} = this
+      let {span, offset,ipad,narrowPc,pc,widePc} = this
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        phone && [`col-phone-${phone.span}`],
         ipad && [`col-ipad-${ipad.span}`],
         narrowPc && [`col-narrow-pc-${narrowPc.span}`],
         pc && [`col-pc-${pc.span}`],
@@ -64,21 +62,6 @@ $class-prefix: offset-;
 @for $n from 1 through 24 {
   .#{$class-prefix}#{$n} {
     margin-left: ($n/24)*100%
-  }
-}
-
-@media (max-width: 576px) {
-  $class-prefix: col-phone-;
-  @for $n from 1 through 24 {
-    .#{$class-prefix}#{$n} {
-      width: ($n/24)*100%
-    }
-  }
-  $class-prefix: offset-phone-;
-  @for $n from 1 through 24 {
-    .#{$class-prefix}#{$n} {
-      margin-left: ($n/24)*100%
-    }
   }
 }
 
