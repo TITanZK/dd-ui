05e2a698eee78953a48f5517e6515d69c0774ea6
TITanZK <503838819@qq.com>
2021-03-31 16:16:39 +0800

    sider 暂时去掉简单动画

--- src/Sider.vue
+++ src/Sider.vue
@@ -1,31 +1,15 @@
 <template>
-  <transition name="slide">
-    <div class="sider" v-if="visible">
-      <slot></slot>
-      <icon name="right" @click.native="visible = false"></icon>
-    </div>
-  </transition>
+  <div class="sider">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-import Icon from "./Icon"
-
 export default {
   name: "Sider",
-  components: {Icon},
-  data() {
-    return {visible: true}
-  },
-  methods: {}
 }
 </script>
 
 <style scoped lang="scss">
-.sider {
-  position: relative;
-  > .d-icon {width: 1.5em;height: 1.5em;
-    position: absolute;top: 0;right: 0;}
-}
 
-.slide-enter-active, .slide-leave-active {transition: all .4s;}
 </style>
