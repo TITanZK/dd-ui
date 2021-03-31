8650a08f6637a793003bf1af3b6ed1671304ae79
TITanZK <503838819@qq.com>
2021-03-31 17:01:59 +0800

    添加 toast 组件，引入 plugin

--- src/app.js
+++ src/app.js
@@ -10,26 +10,35 @@ import Sider from "./Sider"
 import Footer from "./Footer"
 import Content from "./Content"
 import Header from "./Header"
+import Toast from "./Toast"
+import plugin from "./plugin"
 
-
-Vue.component('d-button',Button)
-Vue.component('d-icon',Icon)
-Vue.component('d-button-group',ButtonGroup)
-Vue.component('d-input',Input)
-Vue.component('d-row',Row)
-Vue.component('d-col',Col)
-Vue.component('d-layout',Layout)
-Vue.component('d-sider',Sider)
-Vue.component('d-footer',Footer)
-Vue.component('d-content',Content)
-Vue.component('d-header',Header)
-
+Vue.component('d-button', Button)
+Vue.component('d-icon', Icon)
+Vue.component('d-button-group', ButtonGroup)
+Vue.component('d-input', Input)
+Vue.component('d-row', Row)
+Vue.component('d-col', Col)
+Vue.component('d-layout', Layout)
+Vue.component('d-sider', Sider)
+Vue.component('d-footer', Footer)
+Vue.component('d-content', Content)
+Vue.component('d-header', Header)
+Vue.component('d-toast', Toast)
+Vue.use(plugin)
 new Vue({
   el: '#app',
-  data:{
+  data: {
     loading1: false,
     loading2: true,
     loading3: false,
-    message:'你好'
+    message: '你好'
+  },
+  created() {
+  },
+  methods: {
+    showToast() {
+      this.$toast('我是 message')
+    }
   }
 })
