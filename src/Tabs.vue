16ff43a1cf044b5747185b90f9df24a04d96ebc5
TITanZK <503838819@qq.com>
2021-04-03 19:28:54 +0800

    删除手误代码

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -29,13 +29,12 @@ export default {
     this.$children.forEach(vm => {
       if (vm.$options.name === 'Tabs-head') {
         vm.$children.forEach(childVm => {
-          if (childVm.$options.name === 'Tabs-childVm' && childVm.name === this.selected) {
-            this.eventBus.$emit('update:selected',this.selected,childVm)
+          if (childVm.$options.name === 'Tabs-item' && childVm.name === this.selected) {
+            this.eventBus.$emit('update:selected', this.selected, childVm)
           }
         })
       }
     })
-    this.eventBus.$emit('update:selected', this.selected)
   }
 }
 </script>
