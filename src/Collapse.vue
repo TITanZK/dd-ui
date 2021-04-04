c23775574147564eaec5b401d6ef52f5bfb0846c
TITanZK <503838819@qq.com>
2021-04-04 18:56:09 +0800

    collapse 添加 single,存在警告，未解决

--- src/Collapse.vue
+++ src/Collapse.vue
@@ -5,17 +5,36 @@
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: "Collapse"
+  name: "Collapse",
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    }
+  },
+  provide() {
+    if (this.single) {
+      return {
+        eventBus: this.eventBus
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 $border-color: #ddd;
-$border-radius:4px;
+$border-radius: 4px;
 .collapse {
   border: 1px solid $border-color;
   border-radius: $border-radius;
-  border-bottom: none;
 }
 </style>
