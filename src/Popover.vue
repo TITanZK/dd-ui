7113ffc4ecbf50a7afc1ecf6317a35ec3b501313
TITanZK <503838819@qq.com>
2021-04-04 11:51:38 +0800

    解决容器有 overflow hidden 时的bug

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -13,14 +15,21 @@ export default {
   data() {
     return {visible: false}
   },
+  mounted() {
+  },
   methods: {
     xxx() {
       this.visible = !this.visible
       if (this.visible === true) {
         this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper)
+          const {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+          this.$refs.contentWrapper.style.left = left + screenX + 'px'
+          this.$refs.contentWrapper.style.top = top + scrollY + 'px'
+
           let eventHandle = () => {
-            this.visible = false// 删除监听器
-            document.removeEventListener('click', eventHandle)
+            this.visible = false
+            document.removeEventListener('click', eventHandle)// 删除监听器
           }
           document.addEventListener('click', eventHandle)
         })
@@ -31,15 +40,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$border-color: #ebeef5;
 .popover {
   display: inline-block;
   position: relative;
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+
+.content-wrapper {
+  position: absolute;
+  transform: translateY(-100%);
+  border: 1px solid $border-color;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
 }
 </style>
