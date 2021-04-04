1b516b396ed3a943ca962830a504efd228a9ed4e
TITanZK <503838819@qq.com>
2021-04-04 10:32:21 +0800

    实现 popover 点击显示隐藏，但还存在bug

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xxx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xxx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -16,6 +16,15 @@ export default {
   methods: {
     xxx() {
       this.visible = !this.visible
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          let eventHandle = () => {
+            this.visible = false// 删除监听器
+            document.removeEventListener('click', eventHandle)
+          }
+          document.addEventListener('click', eventHandle)
+        })
+      }
     }
   }
 }
@@ -25,12 +34,12 @@ export default {
 .popover {
   display: inline-block;
   position: relative;
-  .content-wrapper{
+  .content-wrapper {
     position: absolute;
     bottom: 100%;
     left: 0;
     border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
   }
 }
 </style>
