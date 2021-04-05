959629f9d1c5bce81cc3cb34a797e033406e07b9
TITanZK <503838819@qq.com>
2021-04-05 12:39:04 +0800

    vue press 网站搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'DuoduoUI',
+  description: '一个好用且精美的UI框架',
+  themeConfig: {
+    displayAllHeaders: true, // 默认值：false
+    logo: '/assets/img/hero.png',
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: 'https://google.com'},
+    ],
+    sidebar: [
+      {
+        title: '快速上手',
+        collapsable: false, // 可选的, 默认值是 true,
+        children: [
+          '/get-started/',
+        ]
+      },
+      {
+        title: '组件',
+        collapsable: false, // 可选的, 默认值是 true,
+        children: [
+          '/components/button'
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /assets/img/hero.png
+heroText: 多多 UI
+tagline: 一套为开发者准备的精美组件库
+actionText: 快速上手 →
+actionLink: /get-started/
+features:
+- title: 简单精美
+  details: 以简单易上手为核心，精美的 UI 页面展示做衬托，帮助您打造炫酷的页面。
+- title: Vue 结合
+  details: 目前国内大多数公司使用 Vue 居多，本框架对使用 Vue 的人来说绝对是如虎添翼。
+- title: 质量保障
+  details: 组件库的每一个组件都做了测试，并通过 Travis-CI 持续集成，为您的产品质量保驾护航。
+footer: MIT Licensed | Copyright © 2021-present ZK
+---
