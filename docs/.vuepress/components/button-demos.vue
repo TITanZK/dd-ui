7d06601213e0bc4dded04eb2d2f89d28eb0a66fd
TITanZK <503838819@qq.com>
2021-04-06 08:12:03 +0800

    成功解决 Bug

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -11,16 +11,6 @@ import Button from "../../../src/Button"
 export default {
   components: {
     'd-button': Button,
-  },
-  created() {
-    // console.log('执行了')
-    // console.log(Button)
-  },
-  mounted() {
-    // import('../../../src/Button').then(module=>{
-    // console.log(module)
-    // Vue.use(module)
-    // })
   }
 }
 
