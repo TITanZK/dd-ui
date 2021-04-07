69360a3ae86db196abcc084eabb462d9cca3ac6e
TITanZK <503838819@qq.com>
2021-04-07 15:41:56 +0800

    发布 npm

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 import Button from './src/Button'
 import ButtonGroup from './src/Button-group'
+import Col from "./src/Col"
+import Collapse from "./src/Collapse"
+import CollapseItem from './src/Collapse-item'
+import Content from "./src/Content"
+import Footer from "./src/Footer"
+import Header from "./src/Header"
 import Icon from './src/Icon'
+import Input from "./src/Input"
+import Layout from "./src/Layout"
+import Popover from "./src/Popover"
+import Row from "./src/Row"
+import Sider from "./src/Sider"
+import Tabs from "./src/Tabs"
+import TabsBody from './src/Tabs-body'
+import TabsHead from './src/Tabs-head'
+import TabsItem from './src/Tabs-item'
+import TabsPane from './src/Tabs-pane'
+import Toast from "./src/Toast"
+import Plugin from './src/plugin'
+import Svg from './src/svg'
+import Spread from './src/spread'
 
-export {Button, ButtonGroup, Icon}
+export {
+  Button, ButtonGroup, Col, Collapse,
+  CollapseItem, Content, Footer, Header,
+  Icon, Input, Layout, Popover, Row, Sider,
+  Tabs, TabsBody, TabsHead, TabsItem, TabsPane, Toast, Plugin, Svg, Spread
+}
