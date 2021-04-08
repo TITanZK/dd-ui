816c2a45f4bd6a20854a462d0218d0da3969f042
TITanZK <503838819@qq.com>
2021-04-08 11:04:57 +0800

    添加 deploy 码云一键部署

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy' &&
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:TITanZK/ddui-doc.git master:gh-pages &&
+# git push -f git@github.com:TITanZK/ddui-doc.git master:gh-pages &&
+
+ git push -f git@gitee.com:zk_titan/ddui-doc.git master:gh-pages &&
 
 cd -
